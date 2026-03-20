fdb864f946f8ef3e66daf507fe2b380195cffd12
wraith-klu <saurabhklu.ai@gmail.com>
2026-03-20 16:23:27 +0530

    11: Agent Form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,16 +1,45 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { create } from "domain";
+import { agentsInsertSchema } from "../schemas";
+import { auth } from "@/lib/auth";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const data = await db
-      .select()
-      .from(agents);
+    getOne: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .query(async ({ input }) => {
+            const [existingAgent] = await db
+                .select()
+                .from(agents)
+                .where(eq(agents.id, input.id));
 
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    // throw new TRPCError({ code: "BAD_REQUEST" });
+            return existingAgent;
+        }),
+    getMany: protectedProcedure.query(async () => {
+        const data = await db
+            .select()
+            .from(agents);
 
-    return data;
-  }),
+        // await new Promise((resolve) => setTimeout(resolve, 5000));
+        // throw new TRPCError({ code: "BAD_REQUEST" });
+
+        return data;
+    }),
+    create: protectedProcedure
+        .input(agentsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+            const [createdAgent] = await db
+                .insert(agents)
+                .values({
+                    ...input,
+                    // userId: ctx.auth.user.id,
+                    userId: ctx.session.user.id,
+                })
+                .returning();
+
+            return createdAgent;
+        }),
 });
