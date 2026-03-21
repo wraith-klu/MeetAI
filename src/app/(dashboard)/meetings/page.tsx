7851091ca1cdb25823ff035e935c66c68690deea
wraith-klu <saurabhklu.ai@gmail.com>
2026-03-22 00:39:28 +0530

    16: Meetings setup

---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,26 @@
+import { MeetingsView } from "@/modules/meetings/ui/views/meetings-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/views/meetings-view";
+
+
+
+const Page = () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { agentsRouter } from "@/modules/agents/server/procedures";
 import { createTRPCRouter } from "../init";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter,
 });
 
 // export type definition of API
