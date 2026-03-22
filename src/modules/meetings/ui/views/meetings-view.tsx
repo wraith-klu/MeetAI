181bc6b7e58a5d25bd3a17c15281bc0f9578bc6f
wraith-klu <saurabhklu.ai@gmail.com>
2026-03-22 19:12:50 +0530

    18: Meetings data table

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -4,32 +4,42 @@ import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "@/components/data-table";
+import { meetings } from "@/db/schema";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const MeetingsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
   return (
-    <div>
-      {JSON.stringify(data)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          description="Schedule a meeting to connect with others. Each meeting lets you collabrate, share ideas, and intract with participants in real-time."
+        />
+      )}
     </div>
   );
 };
 
 export const MeetingsViewLoading = () => {
-    return (
-        <LoadingState
-            title="Loading Meetings"
-            description="This may take a few seconds"
-        />
-    );
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="This may take a few seconds"
+    />
+  );
 };
 
 export const MeetingsViewError = () => {
-    return (
-        <ErrorState
-            title="Error Loading Meetings"
-            description="Something went wrong"
-        />
-    );
+  return (
+    <ErrorState
+      title="Error Loading Meetings"
+      description="Something went wrong"
+    />
+  );
 };
