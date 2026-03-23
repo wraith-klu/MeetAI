04e54368ce7459ca4b012738a61879714e895d45
wraith-klu <saurabhklu.ai@gmail.com>
2026-03-24 03:25:05 +0530

    22: Video call (Steam)

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,23 @@ import path from "path";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-auth"],
 
+  experimental: {
+    esmExternals: "loose",
+  },
+
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       zod: path.resolve(__dirname, "node_modules/zod"),
+      "@mediapipe/tasks-vision": false, // 🔥 fix your error
     };
+
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      os: false,
+    };
+
     return config;
   },
 };
